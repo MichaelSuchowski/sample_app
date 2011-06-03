c3f42e61da056ea7311a2b89a2a40d3cf301eaa5
MichaelSuchowski <michael@suchowskilaw.com>
2011-06-02 21:17:38 -0400

    Users index with pagination

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,6 +19,9 @@ describe UsersController do
   			@user = test_sign_in(Factory(:user))
   			Factory(:user,  :email => "another@example.com")
   			Factory(:user,  :email => "another@example.net")
+  			
+  		30.times do
+  			Factory(:user, :email => Factory.next(:email))
   		end
   		
   		it "should be successful" do
@@ -29,19 +32,27 @@ describe UsersController do
   		it "should have the right title" do
   			get  :index
   			response.should have_selector('title', :content=> "All Users")
-  			end
+  		end
   			
   		it "should have an element for each user" do
   			get	:index
-  			User.all.each do |user|
+  			User.paginate(:page => 1).each do |user|
   				response.should have_selector('li', :content => user.name)
-  				
-  				end
-  				
-  				
-  			end	
-  		end
-  	end  		
+  		end		
+  	  end
+  	  
+  	  it "should paginate users" do
+  	  	get :index
+  	  	response.should have_selector('div.pagination')
+  	  	response.should have_selector('span.disabled', :content => "Previous")
+  	  	response.should have_selector('a', 	:href => "/users?page=2",
+  	  										:content => "2")
+  	  	response.should have_selector('a', 	:href => "/users?page=2",
+  	  										:content => "Next")
+  	  	end
+  	end	
+  end
+end  		
   	
   	
   describe "GET 'show'" do
--- spec/factories.rb
+++ spec/factories.rb
@@ -4,3 +4,9 @@ Factory.define :user do |user|
 	user.password				"foobar"
 	user.password_confirmation  "foobar"
 end
+
+Factory.sequence :email do |n|
+	"person-#{n}@example.com"
+	
+end
+	
